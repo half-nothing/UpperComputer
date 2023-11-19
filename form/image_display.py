4466be3be193f963a80173c3ea0edcc8e95a7308
Half_nothing <Half_nothing@163.com>
2023-11-20 01:03:29 +0800

    构建摄像头调试界面

---
+++ form/image_display.py
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>ImageDisplay</class>
+ <widget class="QWidget" name="ImageDisplay">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>1000</width>
+    <height>1000</height>
+   </rect>
+  </property>
+  <property name="minimumSize">
+   <size>
+    <width>1000</width>
+    <height>1000</height>
+   </size>
+  </property>
+  <property name="windowTitle">
+   <string>Form</string>
+  </property>
+  <widget class="QLabel" name="pos">
+   <property name="geometry">
+    <rect>
+     <x>10</x>
+     <y>10</y>
+     <width>53</width>
+     <height>16</height>
+    </rect>
+   </property>
+   <property name="text">
+    <string/>
+   </property>
+  </widget>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>
---
+++ module/components/widget/image_display.py
@@ -0,0 +1,9 @@
+from PyQt6.QtWidgets import QWidget
+from form.generate.image_display import Ui_ImageDisplay
+
+
+class ImageDisplay(QWidget, Ui_ImageDisplay):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.setupUi(self)
