b68f149cdf5906ece74c5539e3a22a60c7c215f3
Half_nothing <Half_nothing@163.com>
2023-11-26 00:39:42 +0800

    修改主题

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 
-from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QColor, QIcon, QPalette
+from PyQt6.QtWidgets import QApplication, QStyleFactory
 
 from module.main_window import MainWindow
 from module.utils.path_solve import get_resource_path
@@ -9,6 +10,15 @@ from module.utils.path_solve import get_resource_path
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setWindowIcon(QIcon(get_resource_path('resource/icon/icon.ico')))
+    app.setStyle(QStyleFactory.create("Fusion"))
+    palette = QPalette()
+    palette.setColor(QPalette.ColorRole.Window, QColor(53, 53, 53))
+    palette.setColor(QPalette.ColorRole.WindowText, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.Button, QColor(53, 53, 53))
+    palette.setColor(QPalette.ColorRole.ButtonText, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.Highlight, QColor(142, 45, 197))
+    palette.setColor(QPalette.ColorRole.HighlightedText, Qt.GlobalColor.white)
+    app.setPalette(palette)
     application = MainWindow()
     application.show()
     sys.exit(app.exec())
